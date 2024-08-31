75434ae74146a3ddb549522f5b10ff9c1bf78207
ArthurVaso <arthur.v@aluno.ifsp.edu.br>
2024-08-31 16:43:53 -0300

    clases para login

--- src/main/java/com/futebol/gestao_time/controller/LoginController.java
+++ src/main/java/com/futebol/gestao_time/controller/LoginController.java
@@ -3,11 +3,6 @@ package com.futebol.gestao_time.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.futebol.gestao_time.utils.Resposta;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("login")
@@ -16,7 +11,6 @@ public class LoginController {
 
     // @PostMapping("/login")
     // public Resposta login(@RequestBody UsuarioDTO usuarioDto) {
-    //     //TODO: process POST request
         
     //     return null;
     // }
